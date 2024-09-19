ade4ef361a09db8807e7c46c0c954703db14873e
Loricko9 <lesaulnierlorick@gmail.com>
2024-09-19 20:32:45 +0200

    reorgenize template file

--- transcendance_srcs/frontend/views.py
+++ transcendance_srcs/frontend/views.py
@@ -5,5 +5,5 @@ from django.template import loader
 # Create your views here.
 def index(request):
 	context = {}
-	template = loader.get_template('frontend/index.html')
+	template = loader.get_template('index.html')
 	return (HttpResponse(template.render(context, request)))
