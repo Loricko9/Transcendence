76c1278e62457c0561e4d373b279b609a45a9d39
Loricko9 <lesaulnierlorick@gmail.com>
2024-10-05 20:14:03 +0200

    design index page & 404 page

--- transcendence_srcs/frontend/static/js/main.js
+++ transcendence_srcs/frontend/static/js/main.js
@@ -4,6 +4,12 @@ function redirect_to(url) {
 	router();
 }
 
+// Fonction pour charger les template dans les div
+function loadTemplate(appDiv, Id) {
+	const template = document.getElementById(Id);
+	appDiv.innerHTML = template ? template.innerHTML : "";
+}
+
 // fonction pour rediriger et obtenir le contenu des pages
 function router(){
 	const path = window.location.pathname; //chemin demandé par le user
@@ -11,19 +17,24 @@ function router(){
 	
 	switch (path) {
 		case "/":
-			appDiv.innerHTML = "<h2>C'est ici la page principale !</h2>";
+			loadTemplate(appDiv, "temp_index");
+			appDiv.className = "container-fluid col-md-10 py-2 px-3 my-5";
 			break;
 		case "/page1/":
-			appDiv.innerHTML = "<h2>C'est la page 1</h2>";
+			loadTemplate(appDiv, "temp_page1");
+			appDiv.className = "container-fluid col-md-10 py-2 px-3 my-5";
 			break;
 		case "/page2/":
-			appDiv.innerHTML = "<h2>C'est la superbe page 2</h2>";
+			loadTemplate(appDiv, "temp_page2");
+			appDiv.className = "container-fluid col-md-10 py-2 px-3 my-5";
 			break;
 		case "/page3/":
-			appDiv.innerHTML = "<h2>C'est la magnifique et dernière page</h2>";
+			loadTemplate(appDiv, "temp_page3");
+			appDiv.className = "container-fluid col-md-10 py-2 px-3 my-5";
 			break;
 		default:
-			appDiv.innerHTML = "<h2>Page non trouvée !! PRANKED</h2>";
+			loadTemplate(appDiv, "temp_notFound");
+			appDiv.className = "container-fluid col-md-7 py-2 px-3 my-5";
 	}
 }
 
@@ -42,3 +53,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //gère le retour arrière/avant dans l'historique (les flèches)
 window.addEventListener('popstate', router);
+
+function Click_login() {
+    const dropdownElement = document.getElementById('dropdown_form');    
+    const dropdown = new bootstrap.Dropdown(dropdownElement);
+    dropdown.show();
+}
