1fda6c1a159d418ae9696cf4b667849244a9234b
Loricko9 <lesaulnierlorick@gmail.com>
2024-10-12 16:50:48 +0200

    Add lang button (responsive not fix)

--- transcendence_srcs/frontend/static/js/main.js
+++ transcendence_srcs/frontend/static/js/main.js
@@ -1,4 +1,4 @@
-//Fonction qui permet de ne pas recharger la page (1ère appeler)
+// Fonction qui permet de ne pas recharger la page (1ère appeler)
 function redirect_to(url) {
 	history.pushState(null, null, url);
 	router();
@@ -10,13 +10,12 @@ function loadTemplate(appDiv, Id) {
 	appDiv.innerHTML = template ? template.innerHTML : "";
 }
 
-// fonction pour rediriger et obtenir le contenu des pages
+// Fonction pour rediriger et obtenir le contenu des pages
 function router(){
-	const path = window.location.pathname; //chemin demandé par le user
-	const appDiv = document.getElementById("app"); //selectionne le div 'app' pour ajouter des truc dedans 
-	
-	const new_path = path.substring(3);
-	switch (new_path) {
+	const path = window.location.pathname.substring(3); //chemin demandé par le user
+	const appDiv = document.getElementById("app"); // selectionne le div 'app' pour ajouter des truc dedans 
+
+	switch (path) {
 		case "/":
 			loadTemplate(appDiv, "temp_index");
 			appDiv.className = "container-fluid col-md-10 py-2 px-3 my-5";
@@ -39,25 +38,60 @@ function router(){
 	}
 }
 
-//fonction pour detecter les click sur le liens
+// Fonction pour detecter les click sur le liens / chargement page
 document.addEventListener("DOMContentLoaded", () => {
+	let lang = Get_Cookie("language");
+	if (lang != null) {
+		switch (lang) {
+			case "fr":
+				document.getElementById("btn_fr").classList.add("active")
+				break;
+			case "en":
+				document.getElementById("btn_en").classList.add("active")
+				break;
+			case "es":
+				document.getElementById("btn_es").classList.add("active")
+				break;
+			default:
+				break;
+		}
+	}
+	
+	// partie pour gerer empecher les <a data-link> de recharger la page
 	document.querySelectorAll('a[data-link]').forEach(link => {
 		link.addEventListener('click', (event) => {
-			event.preventDefault(); //evite le rechargement de la page
-			const target = event.target.getAttribute('href');
+			event.preventDefault(); // evite le rechargement de la page
+			const target = event.currentTarget.getAttribute('href');
 			const lang_path = window.location.pathname.substring(0, 3);
 			redirect_to(lang_path + target); // vers la nouvelle page
 		});
 	});
 	
-	router(); //gère la cas pour rafraichir la page
+	router(); // gère la cas pour rafraichir la page
 });
 
-//gère le retour arrière/avant dans l'historique (les flèches)
+// gère le retour arrière/avant dans l'historique (les flèches)
 window.addEventListener('popstate', router);
 
 function Click_login() {
     const dropdownElement = document.getElementById('dropdown_form');    
     const dropdown = new bootstrap.Dropdown(dropdownElement);
     dropdown.show();
+}
+
+function Change_lang(lang) {
+	const path = window.location.pathname.substring(3);
+	window.location.href = "/api/lang/" + lang + "?prev=" + path
+}
+
+// Fonction pour recuperer la valeur d'un cookie
+function Get_Cookie(name) {
+	let new_name = name + "=";
+	let tab = decodeURIComponent(document.cookie).split(';');
+	for (let i = 0; i < tab.length; i++) {
+		let cookie = tab[i].trim();
+		if (cookie.indexOf(new_name) == 0)
+			return (cookie.substring(new_name.length, cookie.length))
+	}
+	return null
 }
