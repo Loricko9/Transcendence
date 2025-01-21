afaafde2119fdf145ed1a4be597af4b760664987
Loricko9 <lesaulnierlorick@gmail.com>
2025-01-21 12:33:56 +0100

    erase secret key

--- transcendence_srcs/transcendence_srcs/settings.py
+++ transcendence_srcs/transcendence_srcs/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p137f&8iautjsybygl)_3kc0(r)(+smeu3_v^r%13csbdg--&^'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
